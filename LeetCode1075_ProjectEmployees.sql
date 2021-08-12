1789f883f09d89c1efd6eb513959846e3907431d
debanjan89 <deray@microsoft.com>
2021-08-13 00:36:48 +0530

    LeetCode 20/70

---
+++ LeetCode1075_ProjectEmployees.sql
@@ -0,0 +1,147 @@
+-- Table: Project
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Table: Employee
+
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table.
+ 
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+-- The query result format is in the following example:
+
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 1                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+
+-- Result table:
+-- +-------------+---------------+
+-- | project_id  | average_years |
+-- +-------------+---------------+
+-- | 1           | 2.00          |
+-- | 2           | 2.50          |
+-- +-------------+---------------+
+-- The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+
+SELECT project_id, AVG(experience_years)
+FROM Project as p 
+INNER JOIN Employee as e 
+ON p.employee_id=e.employee_id
+GROUP BY project_id
+
+-- Write an SQL query that reports all the projects that have the most employees.
+
+-- The query result format is in the following example:
+
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 1                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+
+-- Result table:
+-- +-------------+
+-- | project_id  |
+-- +-------------+
+-- | 1           |
+-- +-------------+
+-- The first project has 3 employees while the second one has 2.
+
+SELECT TOP 1 project_id
+FROM (
+    SELECT project_id, COUNT(employee_id) AS Cnt
+    FROM Project 
+    GROUP BY project_id    
+) AS A 
+ORDER BY Cnt DESC
+
+-- Write an SQL query that reports the most experienced employees in each project. 
+-- In case of a tie, report all employees with the maximum number of experience years.
+
+-- The query result format is in the following example:
+
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 3                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+
+-- Result table:
+-- +-------------+---------------+
+-- | project_id  | employee_id   |
+-- +-------------+---------------+
+-- | 1           | 1             |
+-- | 1           | 3             |
+-- | 2           | 1             |
+-- +-------------+---------------+
+-- Both employees with id 1 and 3 have the 
+-- most experience among the employees of the first project. For the second project, the employee with id 1 has the most experience.
+
+SELECT project_id, employee_id
+FROM (
+    SELECT p.project_id, p.employee_id, RANK() OVER(PARTITION BY project_id ORDER BY experience_years DESC) AS Rnk
+    FROM Project as p 
+    INNER JOIN Employee as e 
+    ON p.employee_id = e.employee_id
+) AS A 
+WHERE Rnk=1
